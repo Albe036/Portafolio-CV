2209735797a873324b212140f014b13b70267267
Albeiro Lozano <albeiro036@gmail.com>
2021-01-17 00:18:18 -0500

    responsive 520

--- components/nav_footer.js
+++ components/nav_footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Navegation from './navegation';
 import Footer from './footer';
 import Head from 'next/head';
+import Style from '../styles/global.module.css';
 
 const Nav_footer = (props) =>{
     return(
-        <div>
+        <div className={Style.contenedor_nav_footer}>
             <Head>
                 <title>{props.title}</title>
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
